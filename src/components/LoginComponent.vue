1eb55bd895262aa60baa29c415ed5c797a6310f0
DDundara <116847625+DDundara@users.noreply.github.com>
2023-08-10 22:19:54 +0200

    login component

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,29 +1,23 @@
 <template>
-  <div class="login-container">
-    <h2>Login</h2>
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          v-model="username"
-          class="form-control"
-          required
-        />
-       
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          class="form-control"
-          required
-        />
-      </div>
-      <button type="submit" class="btn btn-primary">Login</button>
-    
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 shadow-md rounded-md w-96">
+      <h2 class="text-3xl font-semibold mb-4">Log in</h2>
+      <form @submit.prevent="login" class="space-y-4">
+        <div>
+          <label class="block text-sm font-medium">Username</label>
+          <input v-model="username" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" type="text" />
+        </div>
+        <div>
+          <label class="block text-sm font-medium">Password</label>
+          <input v-model="password" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" type="password" />
+        </div>
+        <div>
+          <label class="block text-sm font-medium">Confirm Password</label>
+          <input v-model="confirmPassword" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500" type="password" />
+        </div>
+        <button class="w-full bg-blue-500 text-white rounded-md py-2 hover:bg-blue-600">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -33,39 +27,16 @@ export default {
     return {
       username: '',
       password: '',
+      confirmPassword: ''
     };
-  }
- 
+  },
+  methods: {
+    login() {
+      console.log(this.username);
+    },
+  },
 };
 </script>
 
-<style>
-.login-container {
-  max-width: 300px;
-  margin: 0 auto;
-  padding: 40px;
-  border: 5px solid #ccc;
-  border-radius: 5px;
-  background-color: white ;
-}
-
-.form-group {
-  margin-bottom: 20px;
-}
-
-.form-control {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-}
-
-.btn-primary {
-  background-color: blue;  
-  color: white; 
-  border: none;
-  border-radius: 4px;
-  padding: 10px 20px;
-  cursor: pointer;
-}
+<style scoped>
 </style>
